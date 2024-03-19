15ac91348d0d75176dd54ae37d8a51c50fead574
Jared Ryan Gibson <jared_gibson@byu.edu>
2024-03-18 22:29:10 -0600

    Fix tip submit bug

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -31,13 +31,13 @@ export class AuthGuard implements CanActivate {
       context.getClass(),
     ]);
 
-    if (isPublic) {
-      return true;
-    }
-
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
+      if (isPublic) {
+        return true;
+      }
+
       throw new UnauthorizedException();
     }
     try {
