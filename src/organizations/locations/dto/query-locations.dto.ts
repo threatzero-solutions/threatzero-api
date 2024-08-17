030bf348a369a528bd7671fc72e385669226f43e
Jared Ryan Gibson <jared_gibson@byu.edu>
2024-08-17 13:19:22 -0600

    Update location query

--- src/organizations/locations/dto/query-locations.dto.ts
+++ src/organizations/locations/dto/query-locations.dto.ts
@@ -1,6 +1,46 @@
+import { Type } from 'class-transformer';
+import {
+  IsIn,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import {
+  BaseQueryOrderDto,
+  QueryOrder,
+  QueryOrderOptions,
+} from 'src/common/dto/base-query-order.dto';
 import { BaseQueryDto } from 'src/common/dto/base-query.dto';
 
+export class LocationsQueryOrderDto extends BaseQueryOrderDto {
+  @IsOptional()
+  @IsIn(QueryOrderOptions)
+  ['unit.name']: QueryOrder;
+
+  @IsOptional()
+  @IsIn(QueryOrderOptions)
+  name: QueryOrder;
+
+  @IsOptional()
+  @IsIn(QueryOrderOptions)
+  locationId: QueryOrder;
+}
+
 export class QueryLocationsDto extends BaseQueryDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => LocationsQueryOrderDto)
+  order: LocationsQueryOrderDto = new LocationsQueryOrderDto();
+
+  @IsOptional()
+  @IsUUID('4', { each: true })
+  ['unit.id']?: string | string[];
+
+  @IsOptional()
+  @IsString({ each: true })
+  ['unit.slug']?: string | string[];
+
   protected getSearchFields() {
     return ['name', 'locationId'];
   }
