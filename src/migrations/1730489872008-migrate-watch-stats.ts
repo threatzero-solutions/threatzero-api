9aec250919ec0c43963539e6e388f797e135737e
Jared Gibson <jared.gibson@live.com>
2024-11-01 14:30:30 -0600

    Hopefully fix migration

--- src/migrations/1730489872008-migrate-watch-stats.ts
+++ src/migrations/1730489872008-migrate-watch-stats.ts
@@ -14,21 +14,21 @@ export class MigrateWatchStats1730489872008 implements MigrationInterface {
             "completedOn",
             "url"
          )
-         SELECT DISTINCT ON("watch_stat"."userExternalId", "watch_stat"."trainingItemId", "course_enrollment"."id")
-            "watch_stat"."userExternalId",
-            "watch_stat"."email",
-            "watch_stat"."trainingItemId",
-            "watch_stat"."organizationId",
-            "course_enrollment"."id",
-            COALESCE("watch_stat"."percentWatched", 0) / 100,
-			"watch_stat"."percentWatched" > 85,
-			CASE WHEN "watch_stat"."percentWatched" > 85 THEN NOW() ELSE NULL END,
-            ''
-         FROM "watch_stat"
-         LEFT JOIN "course_enrollment"
-         ON ("watch_stat"."trainingCourseId" IS NULL OR
-		 		"watch_stat"."trainingCourseId" = "course_enrollment"."courseId"::TEXT) AND
-		 	"course_enrollment"."organizationId" = "watch_stat"."organizationId"
+      SELECT DISTINCT ON("watch_stat"."userExternalId", "watch_stat"."trainingItemId", "course_enrollment"."id")
+        "watch_stat"."userExternalId",
+        "watch_stat"."email",
+        "watch_stat"."trainingItemId",
+        "watch_stat"."organizationId",
+        "course_enrollment"."id",
+        COALESCE("watch_stat"."percentWatched", 0) / 100,
+        COALESCE("watch_stat"."percentWatched", 0) > 85,
+        CASE WHEN "watch_stat"."percentWatched" > 85 THEN NOW() ELSE NULL END,
+        ''
+      FROM "watch_stat"
+      LEFT JOIN "course_enrollment"
+      ON ("watch_stat"."trainingCourseId" IS NULL OR
+        "watch_stat"."trainingCourseId" = "course_enrollment"."courseId"::TEXT) AND
+          "course_enrollment"."organizationId" = "watch_stat"."organizationId"
 		 LEFT JOIN "training_course"
 		 ON "training_course"."id" = "course_enrollment"."courseId"
 		 LEFT JOIN "training_section"
@@ -38,7 +38,8 @@ export class MigrateWatchStats1730489872008 implements MigrationInterface {
          WHERE
             "watch_stat"."organizationId" IS NOT NULL AND
             "course_enrollment"."id" IS NOT NULL AND
-			"training_section_item"."itemId" = "watch_stat"."trainingItemId"`,
+			"training_section_item"."itemId" = "watch_stat"."trainingItemId"
+      ON CONFLICT ("userId", "enrollmentId", "itemId") DO NOTHING;`,
     );
   }
 
